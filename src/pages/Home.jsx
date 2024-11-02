b356552a31cbb7b33519d25bf0be6677a134c025
boribam <borissal11@gmail.com>
2024-11-02 19:09:46 +0900

    p 요소 추가했습니다.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
     <HomeContainer>
       <Title>홈 페이지</Title>
       <p>여기에 홈 페이지 관련 내용을 추가하세요.</p>
-      <p>홈페이지를 만들어보겠습니다.</p>
+      <p>홈페이지를 만들어보겠습니다...</p>
     </HomeContainer>
   );
 };
